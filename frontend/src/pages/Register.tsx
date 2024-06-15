e7c10a0f796ef3e06f8d8bbbc891a9230c1f6605
Ananto Y.W <ananto@geniox.com>
2024-06-15 06:37:00 +0200

    update readme

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import * as apiClient from '../api-client';
+import { useAppContext } from "../contexts/AppContext";
 
 export type RegisterFormData = {
     firstName: string;
@@ -12,14 +13,17 @@ export type RegisterFormData = {
 
 const Register = () => {
 
+    // import app context on the register
+    const {showToast} = useAppContext();
+
     const {register, watch, handleSubmit, formState: {errors}} = useForm<RegisterFormData>();
 
     const mutation = useMutation(apiClient.register, {
         onSuccess: () => {
-            console.log("registration successful!");
+            showToast({message: "Registration success", type: "SUCCESS"});
         },
         onError: (error: Error) => {
-            console.log(error.message);
+            showToast({message: error.message, type: "ERROR"})
         },
     });
 
